0254dd79b49c36a2ba62eb5116f414c4e12020df
Leonardo Elias <leoelias02@hotmail.com>
2020-02-28 15:08:55 -0300

    Save product in DB and image cloudinary, initial commit list products

--- routes/adminRt.js
+++ routes/adminRt.js
@@ -60,15 +60,70 @@ router.get('/produtos', (req,res) => {
     })
 })
 
-router.get('/produtos/cadastro', (req,res) => {
+router.get('/produtos/cadastro',  (req,res) => {
     res.render('dashboard/produtos/cadastro', {
         layout: 'dashboard/product'
     })
 })
 
-router.get('/produtos/visualizar', (req,res) => {
-    res.render('dashboard/produtos/visualizar', {
-        layout: 'dashboard/product',
+const multer = require('multer');
+const multerConfig = require('../config/multerConfig');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dpbse0jxf',
+    api_key: '355964363781455',
+    api_secret: 'tpMylhf94c8xwK1zfOFpd95BqGQ'
+});
+
+router.post('/produtos/cadastro', multer(multerConfig).single('img'), async (req,res) => {
+    const { name, desc, price } = req.body;
+    // Saving the image in cloudinary
+        if(!req.file) {
+            pathImage = "https://res.cloudinary.com/dpbse0jxf/image/upload/v1582902017/grdfiilp42mhwthwucsq.jpg";
+        } else {
+            pathImage = req.file.path;
+        }
+        await cloudinary.uploader.upload(pathImage, async (err,result) => {
+            if(err) {
+                return console.log(err);
+            }
+            const { url, original_filename } = result;
+            await api.post('/product/cadastrar', {
+                name,
+                desc,
+                price,
+                img: {
+                    url,
+                    originalfile: original_filename,
+                }
+            }).then( (respApi) => {
+                if(respApi.data.status == 0) {
+                    res.render('dashboard/produtos/cadastro', {
+                        layout: 'dashboard/product',
+                    })
+                } else {
+                    // Resposta da api, renderizar no client
+                    res.render('dashboard/produtos/success_cad', {
+                        layout: 'dashboard/product',
+                    })
+                }
+            }).catch( (errApi) => {
+                console.log('Error on send request to API');
+            })
+        })
+})
+
+router.get('/produtos/visualizar', async (req,res) => {
+    await api.get('/product/mostrar', {
+        q: req.query.q,
+    }).then( (respApi) => {
+        res.render('dashboard/produtos/visualizar', {
+            layout: 'dashboard/product',
+            products: respApi.data.products,
+        })
+    }).catch( (errApi) => {
+        console.log('Error on send a request for get Products to API');
     })
 })
 
