a0fc6d718e892b812b511e0302356fa06585fb0b
Leonardo Elias de Oliveira <50466735+leoelias023@users.noreply.github.com>
2020-03-05 16:41:59 -0300

    arrumando main.rt route

--- routes/mainRt.js
+++ routes/mainRt.js
@@ -10,13 +10,12 @@ router.get('/about', (req,res)=>{
     res.render('home/sobre')
 })
 
-<<<<<<< HEAD
 router.get('/404error', (req,res)=>{
     res.render('home/404', {
         layout: "clear"
     })
 })
-=======
+
 router.post('/login' , (req, res, next) => {
     passport.authenticate("local", {
         successRedirect: '/encomenda',
@@ -25,5 +24,4 @@ router.post('/login' , (req, res, next) => {
     })(req, res, next);
 })
 
->>>>>>> f5437aa11de5b5e1043dcbfedfb5f2c189a8fb6f
 module.exports = router;
